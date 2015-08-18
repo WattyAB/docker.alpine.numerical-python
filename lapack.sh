6dcd23e798ea4d00d03b9a11ef4e683e9c91f1b4
Bruno Rocha <rochacbruno@gmail.com>
2015-08-18 19:53:16 -0300

    Update lapack.sh

--- lapack.sh
+++ lapack.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 mkdir -p ~/src/
 cd ~/src/
-RUN mv /tmp/lapack.tgz .
-RUN tar xzf lapack.tgz
-RUN cd lapack-3.5.0/
-RUN cp /tmp/make.inc .
-RUN make lapacklib
-RUN make clean
-RUN export LAPACK=~/src/lapack-3.5.0/liblapack.a
+mv /tmp/lapack.tgz .
+tar xzf lapack.tgz
+cd lapack-3.5.0/
+cp /tmp/make.inc .
+make lapacklib
+make clean
+export LAPACK=~/src/lapack-3.5.0/liblapack.a
